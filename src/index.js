02502670621d9e547a5794eb9e078905d47139af
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2019-03-18 04:35:03 +0530

    clean up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "babel-polyfill";
+import './style.css';
+import M from 'materialize-css';
+
+window.addEventListener('load', ()=>{
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+});
